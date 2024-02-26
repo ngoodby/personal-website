5cd7736dd3b7f6f53f7f314b7744d2ac9fabe82b
ngoodby <46496548+ngoodby@users.noreply.github.com>
2024-02-26 09:07:05 -0800

    fix ally warning

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,3 +1,3 @@
 <div class="flex justify-center mt-4 border rounded">
-	<iframe src="https://resume.creddle.io/embed/8k7yzy501n6" width="850" height="1100" seamless />
+	<iframe title='resume' src="https://resume.creddle.io/embed/8k7yzy501n6" width="850" height="1100" seamless />
 </div>
